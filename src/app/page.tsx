75cb95268c41c591f769cd236e3f0060f1746918
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-08-30 14:29:04 -0500

    create component post and render every posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,31 @@
 "use client"
 import { useState, useEffect } from "react";
-interface Post {
+import Post from "./post/page";
+
+interface PostProps {
   userId?: number;
   id?: number;
   title?: string;
   body?: string;
 }
 
-function Posts(){
-  const [posts, setPosts] = useState<Post[]>([]);
+function Posts() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
   
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(posts => setPosts(posts))
-    console.log(posts)
-  }, [])
+      .then(response => response.json())
+      .then(posts => setPosts(posts));
+  }, []);
 
   return (
-    <div className="grid  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {posts.length === 0 ? (
-        <p>Loading</p>
-      ) : (
-        posts.map(post => (
-          <div className="border border-white rounded-lg p-4 shadow-md" key={post.id}>
-            <h1>{post.id}</h1>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))
-      )}
+    <div className="flex items-center justify-center min-h-screen p-4">
+      <ul className="flex flex-wrap gap-4 justify-center">
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 export default Posts;
-       
---
+++ src/app/post/page.tsx
@@ -0,0 +1,18 @@
+interface PostProps {
+  userId?: number;
+  id?: number;
+  title?: string;
+  body?: string;
+}
+
+const Post = ({post}:{post: PostProps}) => {
+  return (
+    <li className="flex-1 min-w-[300px] rounded-lg p-4 shadow-md border border-white">
+      <h1>{post.id}</h1>
+      <h2>Body: {post.body}</h2>
+      <p>Title: {post.title}</p>
+    </li>
+  );
+}
+
+export default Post;

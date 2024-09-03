38339c807040b4ad9905d6f468517810be8335ce
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-09-03 10:26:07 -0500

    Create Server Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-"use client"
-import { useState, useEffect } from "react";
 import Post from "../components/Post";
 import type { PostProps } from "@/types";
 
-function Posts() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-  
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(posts => setPosts(posts));
-  }, []);
+async function getAllPosts(): Promise<PostProps[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return res.json();
+}
 
+export default async function Posts() {
+  const posts = await getAllPosts();
   return (
     <div className="flex items-center justify-center min-h-screen p-4">
       <ul className="flex flex-wrap gap-4 justify-center">
@@ -22,4 +18,3 @@ function Posts() {
     </div>
   );
 }
-export default Posts;

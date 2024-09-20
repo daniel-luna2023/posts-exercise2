e75745ce9a639ece72cdd52b183d8315115d42e5
Santiago <santiago.garcia.2007.22@gmail.com>
2024-09-20 12:38:54 -0500

    migrate hooks to new next project and migrate folder context to next

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface PostProps {
   userId?: number;
   id?: number;
   title?: string;
   body?: string;
+}
+
+export interface UserProps{
+  id?: number;
+  username?: string;
+  name?: string,
+  email?: string,
+  street?: string,
+  city?: string,
+  zipcode?: string,
+
 }

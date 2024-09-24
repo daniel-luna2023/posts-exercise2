3b860443f68588f9825af044d7d1f099192aca89
Oscar Gomez <oscargomezgomezluna@gmail.com>
2024-09-24 15:24:36 -0500

    POS-7 migrate userDetail

---
+++ src/app/user-detail/[id]/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import api from "@/utils/api";
+import type { UserProps } from "@/types";
+import { useParams } from "next/navigation";
+
+export default function UserDetail() {
+  const { id } = useParams();
+  const [userId, setUserId] = useState<UserProps | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await api.get(`get/user/${id}`);
+        setUserId(response.data);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    };
+    fetchUser();
+  }, [id]);
+  
+  if (!userId) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen px-5">
+      <div className="w-full max-w-md">
+        <h1 className="text-lg font-bold py-3">About you</h1>
+        <div className="border border-slate-200 p-4 rounded-lg shadow">
+          <div className="font-bold flex flex-col gap-1 mb-3 text-xs">
+            <span className="font-normal text-slate-500 text-xs flex flex-col gap-1 mb-2 px-3">{userId.name}</span>
+          </div>
+          <div className="font-bold flex flex-col gap-1 mb-3 text-xs">
+            <span className="font-normal text-slate-500 text-xs flex flex-col gap-1 mb-2 px-3">{userId.username}</span>
+          </div>
+          <div className="font-bold flex flex-col gap-1 mb-3 text-xs">
+            <span className="font-normal text-slate-500 text-xs flex flex-col gap-1 mb-2 px-3">{userId.email}</span>
+          </div>
+          <div className="font-bold flex flex-col gap-1 mb-3 text-xs">
+            <span className="font-normal text-slate-500 text-xs flex flex-col gap-1 mb-2 px-3">
+              {userId.street}, {userId.city}, {userId.zipcode}
+            </span>
+          </div>       
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, ReactNode, SetStateAction } from "react";
 
 export interface PostProps {
   userId?: number;
@@ -8,12 +8,45 @@ export interface PostProps {
 }
 
 export interface UserProps{
+  comments: any;
+  todos: any;
+  posts: any;
+  company: any;
+  website: string;
+  phone: string;
+  address: any;
   id?: number;
   username?: string;
   name?: string,
   email?: string,
   street?: string,
   city?: string,
-  zipcode?: string,
-
+  zipcode?: string, 
 }
+
+export interface Comment {
+  id: number;
+  body: string;
+  postId: number;
+  userId: number;
+}
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+export interface Address {
+  street: string;
+  city: string;
+  zipcode: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
